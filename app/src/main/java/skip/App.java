87d35343a0aba90c4dc54a95eabb7e45dcaf46b0
Dmitry Taran <dmit.taran2018@gmail.com>
2022-11-07 14:20:47 +0200

    myNumbersProcessing method implemented

--- app/src/main/java/skip/App.java
+++ app/src/main/java/skip/App.java
@@ -9,5 +9,10 @@ public class App {
         double b = (40+Math.random()*(50-40));
         System.out.println("Variable a = " + a);
         System.out.println("Variable b = " + b);
+        myNumbersProcessing(a, b);
+    }
+    public static void myNumbersProcessing(double a, double b) {
+        System.out.println("Sum of variables = " + (a+b));
+        System.out.println("Multiplied variables = " + (a*b));
     }
 }
